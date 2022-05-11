62f89db3b1a0b21bdfd8f87265c875aa9435eab1
Rachel Allen <rallenco13@gmail.com>
2022-05-11 17:27:51 -0600

    completed deck class

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,7 +1,8 @@
 const chai = require("chai");
 const expect = chai.expect;
 
-const Deck = require("../src/Turn");
+const Deck = require("../src/Deck");
+const Card = require("../src/Card");
 
 describe("Deck", function() {
   it("should be a function", function() {
@@ -12,5 +13,61 @@ describe("Deck", function() {
   it("should be an instance of deck", function() {
     const deck = new Deck();
     expect(deck).to.be.an.instanceOf(Deck);
+  });
+
+  it("should be able to start with no cards", function() {
+    const deck = new Deck();
+    expect(deck.cards).to.deep.equal([]);
+  });
+
+  it("should be able to store multiple cards", function() {
+    const card1 = new Card(
+      1,
+      "What is Robbie's favorite animal",
+      ["sea otter", "pug", "capybara"],
+      "sea otter"
+    );
+    const card2 = new Card(
+      14,
+      "What organ is Khalid missing?",
+      ["spleen", "appendix", "gallbladder"],
+      "gallbladder"
+    );
+    const card3 = new Card(
+      12,
+      "What is Travis's middle name?",
+      ["Lex", "William", "Fitzgerald"],
+      "Fitzgerald"
+    );
+
+    const deck = new Deck([card1, card2, card3]);
+
+    expect(deck.cards).to.deep.equal([card1, card2, card3]);
+  });
+
+  it("should be able to count the number of cards in the deck", function() {
+    const card1 = new Card(
+      1,
+      "What is Robbie's favorite animal",
+      ["sea otter", "pug", "capybara"],
+      "sea otter"
+    );
+    const card2 = new Card(
+      14,
+      "What organ is Khalid missing?",
+      ["spleen", "appendix", "gallbladder"],
+      "gallbladder"
+    );
+    const card3 = new Card(
+      12,
+      "What is Travis's middle name?",
+      ["Lex", "William", "Fitzgerald"],
+      "Fitzgerald"
+    );
+
+    const deck = new Deck([card1, card2, card3]);
+
+    var countedCards = deck.countCards();
+    expect(countedCards).to.equal(3);
   });
 });
